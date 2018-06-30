93ada4b272844244b4a0a134337d17cf78708625
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-30 16:44:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,8 @@ def badge_maker(name)
 end 
 
 
+def assign_rooms(attendees)
+  room_assignments = []
+  for i in (0...attendees.size)
+    room_assignments << "Hello, #{name}. You'll be assigned to room #{i}."
+end
